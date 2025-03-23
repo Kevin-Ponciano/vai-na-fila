c8d66dbf1d7d900a7297dd4b75f75e3fe1b914b4
Kevin <kevin.ponciano@outlook.com>
2025-03-23 10:46:34 -0300

    tela login + instalação flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,41 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    darkMode: 'selector', // or 'class'
     theme: {
+        colors: {
+            primary: {
+                light: "#BDEDFB", // Azul Claro - Suavidade e Acessibilidade
+                DEFAULT: "#007BFF", // Azul Principal - Confiança e Profissionalismo
+                dark: "#1565C0", // Azul Escuro - Profundidade e Seriedade
+            },
+            secondary: {
+                light: "#FFE082", // Laranja Claro - Otimismo e Destaque Suave
+                DEFAULT: "#FF6F00", // Laranja Principal - Energia e Dinamismo
+                dark: "#E65100", // Laranja Escuro - Força e Impacto
+            },
+            neutral: {
+                light: "#ECEFF1", // Cinza Claro - Suavidade e Legibilidade
+                DEFAULT: "#B0BEC5", // Cinza Médio - Equilíbrio Visual
+                dark: "#37474F", // Cinza Escuro - Sofisticação e Modernidade
+            },
+        },
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
         extend: {
             fontFamily: {
                 poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
